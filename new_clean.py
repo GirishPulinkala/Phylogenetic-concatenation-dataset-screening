1ab0a76d80695faed9c9840b4f76870da1df4416
AndraAlma <95489040+AndraAlma@users.noreply.github.com>
2022-11-23 15:14:12 +0100

    fixed naming
    now includes articlename, year and extra title such as _full, _strict etc.

--- new_clean.py
+++ new_clean.py
@@ -7,7 +7,11 @@ with open("clean_table.tsv", "r") as fh:
     for row in fh:
         line = row.split('\t')
         org_matrix = line[0].split('/')
-        new_id = org_matrix[2]+'_'+line[1]
-        f.write(new_id + '\t' + line[2])
-            
+        extra_name = org_matrix[3].split('.')
+        if org_matrix[2] == extra_name[0]:
+            new_id = org_matrix[2]+'_'+line[1]
+            f.write(new_id + '\t' + line[2])
+        else:
+            new_id = org_matrix[2]+'_'+extra_name[0]+'_'+line[1]
+            f.write(new_id + '\t' + line[2])
 f.close()
