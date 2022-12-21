854c0d8ccce90dbcf06daaf8b1db4d0fe88dfd8a
AndraAlma <95489040+AndraAlma@users.noreply.github.com>
2022-12-21 16:12:21 +0100

    updated naming of outfiles

--- run_sativa.sh
+++ run_sativa.sh
@@ -3,12 +3,12 @@ for f in *.mafft
 do
   base=${f%.mafft}
   echo ${base}
-  python3 change_fasta.py ${base}.mafft ${base}_renamed.fasta ${base}.tax dosReis_table.tsv
+  python3 change_fasta.py ${base}.mafft ${base}_renamed.fasta ${base}.tax dosReis_table.tsv 
   outputString=`python3 sanity_check.py ${base}_renamed.fasta`
   if test -z "$outputString" 
   then 
-    python3 /home/andraalma/sativa.py -s ${base}_renamed.fasta -t ${base}.tax -x ZOO -T 2 -n test_${base} 
-    python3 correct_output.py test_${base}.mis corrected_${base}.mis
+    python3 /home/andraalma/sativa.py -s ${base}_renamed.fasta -t ${base}.tax -x ZOO -T 2 -n ${base}_test
+    python3 correct_output.py ${base}_test.mis ${base}_corrected.mis 
   else 
     echo "Multiple genes! Will not run SATIVA on component ${base}"
     echo "${base}" >> multiple_gene_comp.txt
